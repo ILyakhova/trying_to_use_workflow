d6cff5cc68f330d6e5e21e96bbc1c64bdcef0e7e
Iryna Lyakhova <ippk0629@gmail.com>
2024-06-18 10:37:10 +0300

    fix too long raw

--- src/app.py
+++ src/app.py
@@ -79,7 +79,8 @@ def external_call():
     try:
         response = requests.get(external_url)
         return Response(
-            f"External call response: {response.text}", status=response.status_code
+            f"External call response: {response.text}",
+            status=response.status_code
         )
     except Exception as e:
         return Response(f"Error calling external endpoint: {str(e)}", status=500)
